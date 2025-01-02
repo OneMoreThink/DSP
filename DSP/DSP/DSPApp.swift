39661c2ff3fb7b96517e9ca65bd8adec35ff33cf
fine <92whdtjs@gmail.com>
2025-01-02 22:46:24 +0900

    Project Setting

---
+++ DSP/DSP/DSPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSPApp.swift
+//  DSP
+//
+//  Created by 이종선 on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DSPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
